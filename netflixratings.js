1392e771e0937e034790d12ab436e9b5f9e11316
TianJianan <tianjianan@115.com>
2019-07-24 11:28:16 +0800

    更新注释

--- netflixratings.js
+++ netflixratings.js
@@ -6,6 +6,7 @@ http-response https:\/\/ios\.prod\.ftl\.netflix\.com\/iosui\/user\/.*path=%5B%22
 2.[MITM]分组添加hostname = ios.prod.ftl.netflix.com
 3.电影和剧集的IMDb分数显示位置不一样，剧集会显示在季数后面，电影要滚动页面才显示在顶部标题前面。
 4.因为获取分数的API对中文标题支持不好,所以修改了request,针对该接口返回英文信息,所以可能界面部分信息会变成英文。
+5.API每日有1000次请求限制，如果超出限制请自己申请APIKey替换.(http://www.omdbapi.com/apikey.aspx)
 */
 if ($request.headers != undefined) {
     console.log("request.header" + $request.headers);
