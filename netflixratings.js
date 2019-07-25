cea53b7f632edfb9099a424a01952a51df67e49f
TianJianan <tianjianan@115.com>
2019-07-25 09:31:45 +0800

    update

--- netflixratings.js
+++ netflixratings.js
@@ -4,7 +4,7 @@
 http-request https:\/\/ios\.prod\.ftl\.netflix\.com\/iosui\/user\/.*path=%5B%22videos%22%2C%[0-9]+%22%2C%22summary%22%5D&pathFormat.* script-path=https://raw.githubusercontent.com/ikanam/Surge-Scripts/master/netflixratings.js
 http-response https:\/\/ios\.prod\.ftl\.netflix\.com\/iosui\/user\/.*path=%5B%22videos%22%2C%[0-9]+%22%2C%22summary%22%5D&pathFormat.* requires-body=1, script-path=https://raw.githubusercontent.com/ikanam/Surge-Scripts/master/netflixratings.js
 2.[MITM]分组添加hostname = ios.prod.ftl.netflix.com
-3.电影和剧集的IMDb分数显示位置不一样，剧集会显示在季数后面，电影要滚动页面才显示在顶部标题前面。
+3.IMDb分数将显示在详情页的封面下方。
 4.因为获取分数的API对中文标题支持不好,所以修改了request,针对该接口返回英文信息,所以可能界面部分信息会变成英文。
 5.API每日有1000次请求限制，如果超出限制请自己申请APIKey替换.(http://www.omdbapi.com/apikey.aspx)
 */
@@ -26,8 +26,12 @@ if ($request.headers != undefined) {
             if (obj.Error) {
                 $done({});                   
             } else {
-                bodyObject.value.videos[movieID].volatile.seasonsLabel += `  IMDb: ${obj.imdbRating}`;
-                bodyObject.value.videos[movieID].summary.title = `IMDb: ${obj.imdbRating} ` + title;
+                let supplementalMessage = bodyObject.value.videos[movieID].summary.supplementalMessage;
+                if (supplementalMessage != undefined) {
+                    bodyObject.value.videos[movieID].summary.supplementalMessage = `IMDb Rating: ${obj.imdbRating}  IMDb Votes: ${obj.imdbVotes}\n` + supplementalMessage;
+                } else {
+                    bodyObject.value.videos[movieID].summary.supplementalMessage = `IMDb Rating: ${obj.imdbRating}  IMDb Votes: ${obj.imdbVotes}`;
+                }
                 body = JSON.stringify(bodyObject);
                 $done({body});
             }
